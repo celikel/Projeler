e231476dc0922f23c243c6b7f6d9157602797dec
Fatih ÇELİKEL <fatihcelikel57@gmail.com>
2016-04-26 17:33:59 +0300

    SayiAvi

---
+++ SayiAvi/SayiAvi/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SayiAvi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int sistemSayisi = r.Next(0, 10);
+
+            Console.WriteLine("Sistem 0 ile 10 arasında bir sayı üretti. Bu sayıyı tahmin edin.");
+
+            while (true)
+            {
+                string deger = Console.ReadLine();
+                int sayi = int.Parse(deger);
+
+                if (sayi == sistemSayisi)
+                {
+                    Console.WriteLine("Tebrikler");
+                    break;
+                }
+                else if (sayi < sistemSayisi)
+                {
+                    Console.WriteLine("Yazdığınız değer sistem sayısından küçüktür. Lütfen tekrar deneyin.");
+                }
+                else
+                {
+                    Console.WriteLine("Yazdığınız değer sistem sayısından büyüktür. Lütfen tekrar dene.");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
