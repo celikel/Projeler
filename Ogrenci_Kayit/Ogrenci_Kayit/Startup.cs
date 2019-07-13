cab13e3c99d3baaaa978a6e0ecd4a83e0bb7ab4e
Fatih ÇELİKEL <fatih.celikel@bil.omu.edu.tr>
2019-07-13 20:07:44 +0300

    Ogrenci_Kayit

---
+++ Ogrenci_Kayit/Ogrenci_Kayit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ogrenci_Kayit.Startup))]
+namespace Ogrenci_Kayit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
